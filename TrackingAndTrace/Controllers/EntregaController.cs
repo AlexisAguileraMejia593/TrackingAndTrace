7664a641120ce57c81218b109f0cbb98c1bffbdb
AlexisAguileraMejia593 <alexaguilera992000@gmail.com>
2023-11-23 19:03:42 -0600

    cambios

--- TrackingAndTrace/Controllers/EntregaController.cs
+++ TrackingAndTrace/Controllers/EntregaController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using ML;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -40,7 +43,7 @@ namespace TrackingAndTrace.Controllers
             return View();
         }
         [HttpPost]
-        public ActionResult CrearPaquete(ML.Paquete paquete)
+        public ActionResult CrearPaquete(ML.Paquete paquete, string Email)
         {
             var result = BL.Paquetes.Add(paquete);
             if (result != null)
@@ -51,7 +54,31 @@ namespace TrackingAndTrace.Controllers
             {
                 ViewBag.Mensaje = "No se ha ingresado correctamente el paquete. Error: " + result;
             }
-            return View();
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                Credentials = new NetworkCredential(Email, "utcd alea dwje xqfe"),
+                EnableSsl = true
+            };
+            string mMailServer = "smtp.gmail.com";
+            string mTo = "5577901650@txt.att.net";
+            string mFrom = Email;
+            string mMsg = "Hola, este es un mensaje de texto enviado desde una aplicación C#.";
+            string mSubject = "Mensaje de texto";
+
+            // Crear y enviar el mensaje
+            try
+            {
+                MailMessage message = new MailMessage(mFrom, mTo, mSubject, mMsg);
+                SmtpClient mySmtpClient = new SmtpClient(mMailServer);
+                mySmtpClient.UseDefaultCredentials = true;
+                mySmtpClient.Send(message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al enviar el mensaje: " + ex.Message);
+            }
+
+            return RedirectToAction("Paquetes");
         }
     }
 }
